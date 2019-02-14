bdbe536b0645f1896f974dc30ebb2994b4fac991
Animesh Gaitonde <animesh3436@gmail.com>
2019-02-14 23:41:33 +0530

    Add test cases for ECOM Order Manager

--- ecom-manager/ecomplatform/ordermanager_test.go
+++ ecom-manager/ecomplatform/ordermanager_test.go
@@ -1,7 +1,7 @@
 package ecomplatform
 
 import (
-	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -9,11 +9,37 @@ func TestOrderManager(t *testing.T) {
 	orderManager := &OrderManager{orders: make(map[string]Order)}
 	items := []item{{10, "bread"}, {20, "milk"}, {10, "biscuit"}}
 	requests := []request{{1, items[0]}, {2, items[2]}, {5, items[1]}, {6, items[0]}}
+	requestIds := []string{}
 	orderIds := []string{}
-	requestID := orderManager.createNewOrder(requests[0])
+	for index, request := range requests {
+		_ = index
+		requestID := orderManager.createNewOrder(request)
+		requestIds = append(requestIds, requestID)
+		orderIds = append(orderIds, requestID)
+	}
 
-	orderIds = append(orderIds, requestID)
 	pendingOrders := orderManager.getPendingOrders()
 
-	fmt.Println(len(pendingOrders))
+	if len(pendingOrders) != 4 {
+		t.Errorf("Expected 4 pending orders, actual pending orders %d", len(pendingOrders))
+	}
+
+	amendOrder := amend{4, requestIds[1]}
+	orderManager.modifyOrder(amendOrder)
+
+	orderManager.updateStatus(requestIds[1], "Dispatched")
+	orderManager.updateStatus(requestIds[2], "Cancelled")
+	pendingOrders = orderManager.getPendingOrders()
+
+	if len(pendingOrders) != 2 {
+		t.Errorf("Expected 4 pending orders, actual pending orders %d", len(pendingOrders))
+	}
+
+	if strings.Compare(orderManager.getOrderStatus(requestIds[1]), "Dispatched") != 0 {
+		t.Error("Order status is", orderManager.getOrderStatus(requestIds[1]))
+	}
+
+	if strings.Compare(orderManager.getOrderStatus(requestIds[2]), "Cancelled") != 0 {
+		t.Error("Order status is", orderManager.getOrderStatus(requestIds[1]))
+	}
 }

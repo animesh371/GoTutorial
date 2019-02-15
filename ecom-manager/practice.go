40f2b9c667e27028ce239a04fbac4c576fcec285
Animesh Gaitonde <animesh3436@gmail.com>
2019-02-15 23:30:58 +0530

    Experiment with byte & string data type

--- ecom-manager/practice.go
+++ ecom-manager/practice.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"GoTutorial/ecom-manager/test"
 	"fmt"
 )
 
@@ -11,19 +10,22 @@ func main() {
 	// integerSlices := []int{3, 4, 5, 6}
 	// fmt.Println(integerSlices[2:4])
 
-	countryNames := []string{"India", "Spain", "France", "Germany"}
-	countryNames = append(countryNames, "Japan")
-	// fmt.Println(countryNames)
-	countryNames = append(countryNames, "Australia")
-	// fmt.Println(countryNames)
-	countryNames = append(countryNames, "Greece", "Dombivli")
-	// fmt.Println(countryNames)
-	for index, country := range countryNames {
-		fmt.Printf("Index = %d, Country Name = %s\n", index, country)
-	}
-	// fmt.Println(countryNames)
+	// countryNames := []string{"India", "Spain", "France", "Germany"}
+	// countryNames = append(countryNames, "Japan")
+	// // fmt.Println(countryNames)
+	// countryNames = append(countryNames, "Australia")
+	// // fmt.Println(countryNames)
+	// countryNames = append(countryNames, "Greece", "Dombivli")
+	// // fmt.Println(countryNames)
+	// for index, country := range countryNames {
+	// 	fmt.Printf("Index = %d, Country Name = %s\n", index, country)
+	// }
+	// // fmt.Println(countryNames)
 
-	fmt.Println(test.WordCount("the quick brown dog jumped over the lazy dog"))
-	fmt.Println(test.Greeting())
+	// fmt.Println(test.WordCount("the quick brown dog jumped over the lazy dog"))
+	// fmt.Println(test.Greeting())
 
+	byteStr := []byte("Iis")
+	fmt.Println(len(byteStr), byteStr[0])
+	fmt.Println(string(byteStr))
 }
